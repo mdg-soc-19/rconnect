98bde0e5d0d7f0e1ba0643b7486411fefbe1b1a7
div794 <divyanshagarwal794@gmail.com>
2019-12-05 00:34:46 +0530

    Second page

--- app/src/main/java/com/example/r_connect/MainActivity.java
+++ app/src/main/java/com/example/r_connect/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.r_connect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button)findViewById(R.id.button1);
+        Button btn2 = (Button)findViewById(R.id.button2);
+        btn1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                openSignIn();
+
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                openSignUp();
+
+            }
+        });
+    }
+    public void openSignIn() {
+        Intent intent = new Intent(this, SignInActivity.class);
+        startActivity(intent);
+    }
+
+    public void openSignUp() {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
